62c7263f583ff5727ee353398e0612aad3a22dd4
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2022-10-07 12:51:13 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        System.out.print(A/B);
-        sc.close();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+        System.out.println(A/B);        
     }
 }
