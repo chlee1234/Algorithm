9df90538dbd9747dfb370b0fab785246e7c192d3
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2022-10-07 14:40:17 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        int A = Integer.parseInt(str);
+        System.out.println(A-543);
+    }
+}
