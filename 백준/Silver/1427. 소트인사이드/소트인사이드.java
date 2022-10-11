62895fb0dac17241931a6dba46a00450fee8be4c
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2022-10-11 22:54:07 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,27 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int[] A = new int[str.length()];
-        for(int i = 0; i < str.length(); i++){
-            A[i] = Integer.parseInt(str.substring(i,i+1));
-        }
-        for(int i = 0; i < str.length();i++){
-            int Max = i;
-            for(int j = i+1; j<str.length();j++){
-                if(A[j]>A[Max]) // 내림차순이므로 최댓값을 찾음
-                    Max = j;
-            }
-            if(A[i]<A[Max]){
-                    int temp = A[i];
-                    A[i] = A[Max];
-                    A[Max] = temp;
-            }
-        }
-        for(int i=0;i<str.length();i++){
-            System.out.print(A[i]);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr =  br.readLine().toCharArray();
+        Arrays.sort(arr);
+        for (int i = arr.length - 1; i >= 0; i--){
+            System.out.print(arr[i]);
         }
     }
 }
