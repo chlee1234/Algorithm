86c819ee8c46c413058490f09012c28a9b2083ea
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2022-11-09 18:42:20 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int[] arr = new int[N];
+        int cnt = 0;
+        int v = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] == v) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
