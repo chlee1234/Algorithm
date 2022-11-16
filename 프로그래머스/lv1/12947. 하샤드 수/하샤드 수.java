ee280581ec5dfd2d2fa50b0d666a4870727291c9
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2022-11-16 19:53:07 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        int sum = 0;
+        int i = x;
+        while(x > 0){
+            sum += x%10;
+            x/=10;
+        }
+        if(i % sum == 0){
+            answer = true;
+        } else{
+            answer = false;
+        }
+        return answer;
+    }
+}
