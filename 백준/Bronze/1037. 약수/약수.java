53fd89532d48ee1c1fda7c4e41e92e515db670b9
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2022-12-03 12:27:53 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int[] arr = new int[T];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        for (int i = 0; i < T; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int N = arr[0] * arr[arr.length - 1];
+
+        System.out.println(N);
+    }
+}
