eac0fa9f1150468076b5c33882a894366fc43f50
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2022-12-07 00:10:33 +0900

    [level 2] Title: 전화번호 목록, Time: 237.06 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashSet<String> hs = new HashSet<>();
+
+        for (String phone_number : phone_book) {
+            hs.add(phone_number);
+        }
+        for (String phone_number : phone_book) {
+            for (int i = 0; i < phone_number.length(); i++){
+                if (hs.contains(phone_number.substring(0,i))){
+                    return false;
+                }
+            }
+        }
+        return answer;
+    }
+}
