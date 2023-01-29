611c83b5a5685808e6fb89171204d38aa32ba9d7
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2023-01-29 22:12:51 +0900

    [Bronze I] Title: 최소공배수, Time: 188 ms, Memory: 15060 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            int K = gcd(N, M);
+
+            System.out.println(N * M / K);
+        }
+    }
+
+    public static int gcd(int n, int m) {
+        if (m == 0) {
+            return n;
+        }
+        return gcd(m, n % m);
+    }
+}
