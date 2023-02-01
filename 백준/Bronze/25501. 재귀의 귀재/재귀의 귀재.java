98ed1813ac66e874157a8dd34738cff6c22e6fd0
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2023-02-01 10:39:46 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 336 ms, Memory: 20676 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int cnt;
+    public static int recursion(String s, int l, int r) {
+        cnt++;
+        if (l >= r) return 1;
+        else if (s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l + 1, r - 1);
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            cnt = 0;
+            System.out.println(isPalindrome(br.readLine()) + " " + cnt);
+        }
+    }
+}
