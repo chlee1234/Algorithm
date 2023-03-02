404e5f7fdbeb216c2c8dcaafcb73d4f2fd1516c9
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2023-03-02 14:28:14 +0900

    [Bronze V] Title: 문자열, Time: 148 ms, Memory: 16032 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            String answer = str.charAt(0) + String.valueOf(str.charAt(str.length() - 1));
+            System.out.println(answer);
+        }
+
+        }
+}
