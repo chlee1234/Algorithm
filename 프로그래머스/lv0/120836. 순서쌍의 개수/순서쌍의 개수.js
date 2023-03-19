e8617c7c37cc3e4dd5aa34d848e44feefa9725ac
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2023-03-19 18:35:28 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,9 +1,8 @@
 function solution(n) {
     var answer = 0;
     for (let i = 1; i < Math.sqrt(n); i++) {
-        if (n % i === 0) answer++;
+        if (n % i === 0) answer += 2;
     }
-    answer *= 2;
     if (n % Math.sqrt(n) === 0) answer++;
     return answer;
 }
