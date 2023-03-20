1d49fbc027c61ef73a9f9d3da82667cdeab6c6e4
CHANGHO LEE <107831623+chlee1234@users.noreply.github.com>
2023-03-20 18:23:21 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    var answer = 0;
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] * numbers[j] > answer) answer = numbers[i] * numbers[j];
+        }
+    }
+    return answer;
+}
